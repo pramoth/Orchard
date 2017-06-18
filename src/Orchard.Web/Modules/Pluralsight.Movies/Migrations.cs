eb3f7eeb72fa8054fc321adeb2e3c100a346610a
aaronamm <theeranitp@gmail.com>
2017-06-18 16:04:49 +0700

    - add Movie module - update to TypeScript 2.3 - update TypeScript Promise to version 0.7.7

---
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Migrations.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Pluralsight.Movies
+{
+    public class Migrations : DataMigrationImpl
+    {
+
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
+             builder.WithPart("CommonPart")
+             .WithPart("TitlePart")
+             .WithPart("AutoRoutePart")
+            );
+
+            return 1;
+        }
+    }
+}
