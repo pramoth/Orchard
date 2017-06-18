cbd532cebd4e53c90bd7f5175eee1d5e463b2f57
aaronamm <theeranitp@gmail.com>
2017-06-18 17:01:20 +0700

    altered Movie Content Part

--- src/Orchard.Web/Modules/Pluralsight.Movies/Migrations.cs
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Migrations.cs
@@ -6,6 +6,7 @@ using Orchard.ContentManagement.MetaData;
 using Orchard.ContentManagement.MetaData.Builders;
 using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
+using Newtonsoft.Json;
 
 namespace Pluralsight.Movies
 {
@@ -17,10 +18,45 @@ namespace Pluralsight.Movies
             ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
              builder.WithPart("CommonPart")
              .WithPart("TitlePart")
-             .WithPart("AutoRoutePart")
+             .WithPart("AutoroutePart")
             );
 
             return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
+             builder.WithPart("BodyPart")
+             .Creatable()
+             .Draftable()
+            );
+
+            return 2;
+        }
+
+        //alter part setting
+        public int UpdateFrom2()
+        {
+            var urlPatterns = new []
+            {
+                new {Name = "Movie Title", Pattern = "movies/{Content.Slug}", Description = "movies/movie-title"},
+                new {Name = "Film Title", Pattern = "films/{Content.Slug}", Description = "films/film-title"},
+            };
+            
+            ContentDefinitionManager.AlterTypeDefinition("Movie", builder =>
+             builder
+             .WithPart("BodyPart", partBuilder => 
+                partBuilder.WithSetting("BodyTypePartSettings.Flavor","text"))
+             .WithPart("AutoroutePart", partBuilder =>
+                partBuilder
+                .WithSetting("AutorouteSettings.AllowCustomPattern","true")
+                .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit","false")
+                .WithSetting("AutorouteSettings.PatternDefinitions", JsonConvert.SerializeObject(urlPatterns))
+                .WithSetting("AutorouteSettings.DefaultPatternIndex ","0"))
+            );
+
+            return 3;
         }
     }
 }
