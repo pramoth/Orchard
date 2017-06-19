f1796391bf10643a1e486fb072a44356085cbbba
aaronamm <theeranitp@gmail.com>
2017-06-19 07:12:57 +0700

    displayed Movie Part in the frontend

--- src/Orchard.Web/Modules/Pluralsight.Movies/Drivers/MoviePartDriver.cs
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Drivers/MoviePartDriver.cs
@@ -12,11 +12,20 @@ namespace Pluralsight.Movies.Drivers
     {
         protected override string Prefix => "Movie";
 
+        protected override DriverResult Display(MoviePart part, string displayType, dynamic shapeHelper) {
+            return ContentShape("Parts_Movie", () => shapeHelper.Parts_Movie(MoviePart: part));
+        }
+
         protected override DriverResult Editor(MoviePart part, dynamic shapeHelper)
         {
             return ContentShape("Parts_Movie_Edit", () =>
             shapeHelper.EditorTemplate(TemplateName: "Parts/Movie", Model: part, Prefix: this.Prefix));
 
+        }
+
+        protected override DriverResult Editor(MoviePart part, IUpdateModel updater, dynamic shapeHelper) {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, updater);
         }
     }
 }
