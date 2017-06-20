3237db14ec4efa73becfd9eebeaa36f21dd2426d
aaronamm <theeranitp@gmail.com>
2017-06-20 23:25:18 +0700

    made Taxonomy work with Movie part

--- src/Orchard.Web/Modules/Pluralsight.Movies/Handlers/MovieFeatureEventHandler.cs
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Handlers/MovieFeatureEventHandler.cs
@@ -1,8 +1,9 @@
-﻿using Contrib.Taxonomies.Models;
-using Contrib.Taxonomies.Services;
-using Orchard.ContentManagement;
+﻿using Orchard.ContentManagement;
 using Orchard.Environment;
 using Orchard.Environment.Extensions.Models;
+using Orchard.Logging;
+using Orchard.Taxonomies.Models;
+using Orchard.Taxonomies.Services;
 
 namespace Pluralsight.Movies.Handlers
 {
@@ -11,11 +12,14 @@ namespace Pluralsight.Movies.Handlers
         private readonly ITaxonomyService _taxonomyService;
         private readonly IContentManager _contentManager;
 
+        public ILogger Logger { get; set; }
+
         public MovieFeatureEventHandler(ITaxonomyService taxonomyService,
             IContentManager contentManager)
         {
             _taxonomyService = taxonomyService;
             _contentManager = contentManager;
+            Logger = NullLogger.Instance;
         }
 
         public void Installing(Feature feature)
@@ -32,23 +36,28 @@ namespace Pluralsight.Movies.Handlers
 
         public void Enabled(Feature feature)
         {
+            Logger.Information("movie feature enabled");
             if (_taxonomyService.GetTaxonomyByName("Genre") == null)
             {
+                Logger.Information("creating Genre taxonomy");
                 //create Taxonomy content type as cast to Taxonomy part
                 var genre = _contentManager.New<TaxonomyPart>("Taxonomy");
                 genre.Name = "Genre";
                 _contentManager.Create(genre, VersionOptions.Published);
-                CreateTerm(genre,"Action");
-                CreateTerm(genre,"Adventure");
-                CreateTerm(genre,"Animation");
-                CreateTerm(genre,"Comedy");
-                CreateTerm(genre,"Crime");
-                CreateTerm(genre,"Documentation");
-                CreateTerm(genre,"Drama");
+                CreateTerm(genre, "Action");
+                CreateTerm(genre, "Adventure");
+                CreateTerm(genre, "Animation");
+                CreateTerm(genre, "Comedy");
+                CreateTerm(genre, "Crime");
+                CreateTerm(genre, "Documentation");
+                CreateTerm(genre, "Drama");
+
+                Logger.Information("Genre taxonomy get created");
             }
         }
 
-        private void CreateTerm(TaxonomyPart genre,string genreName) {
+        private void CreateTerm(TaxonomyPart genre, string genreName)
+        {
             var term = _taxonomyService.NewTerm(genre);
             term.Name = genreName;
             _contentManager.Create(term, VersionOptions.Published);
