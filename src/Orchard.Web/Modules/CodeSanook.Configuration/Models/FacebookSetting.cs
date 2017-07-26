395c0536764d0c750c1a0c4fab006fa56306100e
aaronamm <theeranitp@gmail.com>
2017-07-26 23:13:30 +0700

    add model for Configuration project

---
+++ src/Orchard.Web/Modules/CodeSanook.Configuration/Models/FacebookSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSanook.Configuration.Models
+{
+    public class FacebookSetting
+    {
+    }
+}
