b1d0e42eedcba1939c95b978a04389747b4d3193
aaronamm <theeranitp@gmail.com>
2017-09-23 08:26:56 +0700

    Squashed 'src/Orchard.Web/Modules/CodeSanook.Comment/' content from commit 99371a3a9
    git-subtree-dir: src/Orchard.Web/Modules/CodeSanook.Comment
    git-subtree-split: 99371a3a9abbb4e5a9bdfdb6febf54e773008699

---
+++ Models/CommentListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSanook.Comment.Models
+{
+    public class CommentItemViewModel
+    {
+        public ContentItem Comment { get; set; } 
+        public ContentItem User { get; set; } 
+    }
+}
---
+++ sass-watch.bat
@@ -0,0 +1,4 @@
+﻿echo off
+rem convert all SCSS files in Scss folder to CSS file save in Styles Folder  
+rem not create sourcemap
+sass --watch Scss:Styles --sourcemap=none
